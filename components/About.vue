85cf8d0320cb622dcbcb98a2fb74256ba4e0a730
Manuel Obregozo <obregozo.manuel@gmail.com>
2019-12-23 18:50:14 +0100

    feat: refactor resume component

---
+++ components/About.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="self-center flex-1 ">
+    <h1 class="font-bold text-gray-800 text-6xl leading-none">
+      MANUEL
+      <span class="font-bold text-secondary-400">OBREGOZO</span>
+    </h1>
+    <h3 class="font-bold text-gray-600 mb-2 text-2xl">
+      FRONT END DEVELOPER ·
+      <a
+        class="font-bold text-primary-500"
+        href="mailto:obregozo.manuel@gmail.com"
+      >
+        OBREGOZO.MANUEL@GMAIL.COM
+      </a>
+    </h3>
+    <div class="font-bold mb-3 text-gray-500">
+      <i class="fas fa-map-marker-alt"></i> AMSTERDAM, NETHERLANDS
+    </div>
+    <p class="mb-2 mt-8 text-gray-600 font-semibold text-justify">
+      I am an enthusiastic Front End Developer from a little town called
+      Necochea in the coast of Argentina. Passionate about software development
+      and developer communities.
+    </p>
+    <p class="mb-2 text-gray-600 font-semibold text-justify">
+      Currently, I am centered on web, desktop, and hybrid mobile applications,
+      using different JavaScript related Frameworks. I have worked as a
+      developer for more than 8 years, in different kinds of business domains,
+      across a variety of platforms. I executed and contributed to full-stack
+      development projects from design to testing and up to production
+      deployment, with an emphasis on front end features.
+    </p>
+    <p class="text-gray-600 font-semibold text-justify">
+      I consider myself a real team player who understands the importance of
+      teamwork, proactivity, collaboration, continuous feedback, and open
+      communication.
+    </p>
+    <div class="mt-10">
+      <a
+        target="blank"
+        class="mr-4 text-gray-600 hover:text-gray-500"
+        href="https://www.linkedin.com/in/manuelobregozo/?locale=en_US"
+      >
+        <span class="fa-stack fa-lg">
+          <i class="fas fa-circle fa-stack-2x"></i>
+          <i class="fab fa-linkedin-in fa-stack-1x fa-inverse"></i>
+        </span>
+      </a>
+      <a
+        target="blank"
+        class="mr-4 text-gray-600 hover:text-gray-500"
+        href="https://github.com/mobregozo"
+      >
+        <span class="fa-stack fa-lg">
+          <i class="fas fa-circle fa-stack-2x"></i>
+          <i class="fab fa-github-alt fa-stack-1x fa-inverse"></i>
+        </span>
+      </a>
+      <a
+        target="blank"
+        class="mr-4 text-gray-600 hover:text-gray-500"
+        href="https://stackoverflow.com/users/6098430/manuel-obregozo"
+      >
+        <span class="fa-stack fa-lg">
+          <i class="fas fa-circle fa-stack-2x"></i>
+          <i class="fab fa-stack-overflow fa-stack-1x fa-inverse"></i>
+        </span>
+      </a>
+    </div>
+  </div>
+</template>
---
+++ components/Interests.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="my-auto">
+    <h2 class="mb-5 text-6xl font-bold leading-none">Interests</h2>
+    <p class="mb-2 mt-8 text-gray-600 font-semibold text-justify">
+      Besides work, I usually spend my free time outdoors playing sports and
+      travelling around, taking pictures of everything I see.
+    </p>
+    <p class="mb-2 mt-2 text-gray-600 font-semibold text-justify">
+      Related to sports I am passionate about football, love to read and talk
+      about it, and of course play as much as I can.
+    </p>
+    <p class="mb-2 mt-2 text-gray-600 font-semibold text-justify">
+      When I am not running outside, I am behind my drums, instrument that I
+      play since I was a little kid, and I still enjoy playing the good old punk
+      rock songs that I used to listen to when I was at high school.
+    </p>
+    <div class="mt-10 flex flex-wrap items-center justify-around">
+      <div
+        v-for="img in interestPics"
+        :key="img.link"
+        class="hover-text relative"
+      >
+        <div
+          class="description bottom-0 left-0 pl-4 font-semibold pb-2 z-50 text-white"
+        >
+          {{ img.description }}
+        </div>
+        <img
+          :src="`/${img.link}.jpg`"
+          class="img-gallery mx-1 z-10 rounded-lg shadow-lg mt-5 border-solid border-8 border-white"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.hover-text .description {
+  visibility: hidden;
+  position: absolute;
+}
+
+.hover-text:hover .description {
+  visibility: visible;
+}
+.hover-text:hover img {
+  filter: brightness(0.5);
+}
+
+.img-gallery {
+  height: 300px;
+  width: 400px;
+  object-fit: cover;
+}
+</style>
+
+<script>
+export default {
+  data: () => {
+    return {
+      interestPics: [
+        { link: 'beach', description: 'Necochea, Argentina' },
+        { link: 'drums', description: 'Rehearsal' },
+        { link: 'drums1', description: 'Rehearsal' },
+        { link: 'football', description: 'Warming Up' },
+        { link: 'snow', description: 'Snowboarding in Bariloche' },
+        { link: 'snow1', description: 'Snowboarding in Bariloche' }
+      ]
+    }
+  }
+}
+</script>
