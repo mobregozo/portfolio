6da4981adac4e0ab08425f013278757bbb55cb18
Manuel Obregozo <obregozo.manuel@gmail.com>
2019-12-30 19:56:55 +0100

    feat: add lang att to the html tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,13 @@
 export default {
-  mode: 'spa',
+  mode: 'universal',
   /*
    ** Headers of the page
    */
   head: {
     title: 'Manuel Obregozo',
+    htmlAttrs: {
+      lang: 'en'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
