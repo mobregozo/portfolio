b53162520d478bad5e089f4ccabbdcc5c8fda67c
Manuel Obregozo <obregozo.manuel@gmail.com>
2020-12-08 18:46:35 +0100

    feat: add plugins for primisc

---
+++ plugins/link-resolver.js
@@ -0,0 +1,19 @@
+/**
+ * To learn more about Link Resolving check out the Prismic documentation
+ */
+
+export default function(doc) {
+  if (doc.isBroken) {
+    return '/not-found'
+  }
+
+  if (doc.type === 'blog_home') {
+    return '/'
+  }
+
+  if (doc.type === 'post') {
+    return '/thoughts/' + doc.uid
+  }
+
+  return '/not-found'
+}
