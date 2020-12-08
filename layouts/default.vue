2429c8149b72d0e096acd669c81ed000612f7c52
Manuel Obregozo <obregozo.manuel@gmail.com>
2020-12-08 19:27:12 +0100

    fix: add sidebar to the default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>
-    <nuxt />
+  <div
+    class="md:h-screen md:absolute md:top-0 md:bottom-0 md:left-0 md:right-0 md:flex"
+  >
+    <sidebar />
+    <div
+      class="scrollable overflow-x-hidden flex-1 min-h-screen px-6 flex pt-24 md:py-10 w-full max-w-screen-lg"
+    >
+      <nuxt />
+    </div>
   </div>
 </template>
-
+<script>
+import Sidebar from '~/components/Sidebar.vue'
+export default {
+  components: {
+    Sidebar
+  }
+}
+</script>
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  /* font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif; */
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -15,5 +29,40 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+  scroll-behavior: smooth;
+}
+
+body {
+  font-family: 'Open Sans', serif;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: 'Saira Extra Condensed', serif-serif;
+}
+
+.page-enter-active {
+  transition: opacity 0.4s, transform 0.4s;
+  transition-timing-function: ease-in;
+}
+.page-leave-active {
+  transition: opacity 0.2s, transform 0.2s;
+  transition-timing-function: ease-in;
+}
+
+.page-enter-to,
+.page-leave {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+  transform: translateX(2rem);
 }
 </style>
