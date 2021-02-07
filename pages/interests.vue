5032c95a8dbfd48c07946dc7dbbb904d6e32ffeb
Manuel Obregozo <obregozo.manuel@gmail.com>
2021-02-07 18:36:29 +0100

    fix: wording of the interest page

--- pages/interests.vue
+++ pages/interests.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="self-center flex-1 max-w-screen-lg">
     <h2 class="mb-5 text-6xl font-bold">Interests</h2>
-    <p class="mb-2 mt-8 text-gray-700 font-semibold ">
-      Besides work, I usually spend my free time outdoors playing sports and
-      travelling around, taking pictures of everything I see.
+    <p class="my-2 text-gray-700 font-semibold">
+      I was attached to art since I was born, which eventually developed my
+      interest in Frontend Development. Influenced by my mother (who is an art
+      teacher) and my father (who is an engineer) I managed to find the perfect
+      balance between art and logic that I was looking for.
     </p>
-    <p class="mb-2 mt-2 text-gray-700 font-semibold ">
-      Related to sports I am passionate about football, love to read and talk
-      about it, and of course play as much as I can.
+    <p class="my-2 text-gray-700 font-semibold">
+      After the pandemic started, I also discovered that there are two things
+      that give me peace of mind and that I can do in my spare time, which are
+      training and writing. That's when I joined a developer community on
+      Discord called
+      <a
+        href="https://twitter.com/FrontEndCafe"
+        class="font-semibold hover:underline text-primary-700"
+        target="_blank"
+        >@FrontEndCafe </a
+      >, where I usually help new developers and people who change careers to
+      get their first job in the IT world, giving back to the community what I
+      got at the beginning of my career. In addition, there are always people
+      willing to listen when I face any problem.
     </p>
-    <p class="mb-2 mt-2 text-gray-700 font-semibold ">
-      When I am not running outside, I am behind my drums, instrument that I
-      play since I was a little kid, and I still enjoy playing the good old punk
-      rock songs that I used to listen to when I was at high school.
+    <p class="mb-2 mt-8 text-gray-700 font-semibold">
+      Besides work and technology, I usually spend my free time outdoors doing
+      sports and traveling, taking pictures of everything I see.
+    </p>
+    <p class="mb-2 mt-2 text-gray-700 font-semibold">
+      Related to sports, I am passionate about soccer, I love reading and
+      talking about it, and of course, playing as much as I can.
+    </p>
+    <p class="mb-2 mt-2 text-gray-700 font-semibold">
+      When I'm not running outside, I'm behind my drums, an instrument I've
+      played since I was a kid, and I still enjoy playing the old punk rock
+      songs that I used to listen to when I was in high school.
     </p>
     <div class="my-10 grid gap-8 grid-cols-1 md:grid-cols-3">
       <div v-for="img in interestPics" :key="img.link" class="hover-text">
