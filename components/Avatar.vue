0fdbe00d479e7fa9b83f5fa5f56aa6b7e276a30b
Manuel Obregozo <obregozo.manuel@gmail.com>
2021-02-13 14:25:36 +0100

    fix: make the avatr bigger for smaller screens

--- components/Avatar.vue
+++ components/Avatar.vue
@@ -5,7 +5,7 @@
       alt="avatar"
       width="220"
       height="147"
-      class="h-12 w-12 md:h-32 md:w-32 object-cover rounded-full border md:border-4 bg-white"
+      class="h-28 w-28 md:h-32 md:w-32 object-cover rounded-full border md:border-4 bg-white"
     />
   </div>
 </template>
