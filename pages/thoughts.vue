766a82808ae01ffee04e2f54b224ae0a7483215e
Manuel Obregozo <obregozo.manuel@gmail.com>
2021-03-24 19:55:21 +0100

    fix: typo under thoughts

--- pages/thoughts.vue
+++ pages/thoughts.vue
@@ -2,18 +2,18 @@
   <div>
     <h1 class="mb-5 text-6xl font-bold">Thoughts</h1>
     <p class="text-gray-700 font-semibold text-justify">
-      <strong class="font-bold text-gray-800">Opinions are my own! </strong
-      >These are random thoughts that constantly come to my mind, and from time
+      <strong class="font-bold text-gray-800">Opinions are my own! </strong>
+      These are random thoughts that constantly come to my mind, and from time
       to time I translate them into articles.<br />
-      I usually write about aspects of the software that are not technolofy
-      related, but linked to the environment that surrounds us and things we can
-      find there.
+      I usually write about aspects of the software that are not
+      technology-related but linked to the environment that surrounds us and
+      things we can find there.
     </p>
     <p
       class="mt-4 text-gray-700 font-semibold text-sm bg-gray-200 rounded-md p-4"
     >
-      As you may have realised, I am not a native English speaker! <br />
-      So in case you noticed something wrong or that could be improved, please
+      As you may have realized, I am not a native English speaker! <br />
+      So in case, you noticed something wrong or that could be improved, please
       contact me!
     </p>
     <h2 class="text-primary-700 text-5xl mt-8 mb-4 font-semibold">
